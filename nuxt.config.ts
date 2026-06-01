2f7194af162ad8b74fccb8ebbc339a6612f8432c
David Wismer <david@thewismer.ch>
2026-06-01 15:36:01 +0200

    Added pages and components folders + changed nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    baseURL: '/',
+    head: {
+      title: 'ALULA',
+      meta: [
+        { name: 'description', content: '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    }
+  },
+  css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: "static",
+  }
 })
---
+++ pages/exercice/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Exercice</h1>
+</template>
+
+<style scoped>
+
+</style>
